cec46ec071b3f5b5dee29791c654a8664cffc7fa
OrhanBC <orhancangurel@gmail.com>
2023-11-22 16:55:32 -0500

    added replaceMath plugin

--- src/convert-to-pretext.ts
+++ src/convert-to-pretext.ts
@@ -1,4 +1,4 @@
-import { unified } from "unified";
+import { unified, Plugin } from "unified";
 import rehypeStringify from "rehype-stringify";
 import chalk from "chalk";
 import { readFile } from "node:fs/promises";
@@ -13,7 +13,7 @@ import rehypeRemark from "rehype-remark";
 import { toString as hastToString } from "hast-util-to-string";
 import remarkStringify from "remark-stringify";
 // import { visit } from "@unified-latex/unified-latex-util-visit";
-import { visit } from 'unist-util-visit';
+import { visit } from "unist-util-visit";
 import * as Hast from "hast";
 import * as Ast from "@unified-latex/unified-latex-types";
 import { toString } from "@unified-latex/unified-latex-util-to-string";
@@ -30,7 +30,7 @@ function convert(value: string) {
                 signature: "m",
             },
             index: {
-                signature: "m",
+                signature: "o m",
             },
         },
     });
@@ -53,12 +53,19 @@ function convert(value: string) {
             },
             index: (node) => {
                 const args = getArgsContent(node);
+                const formattedArgs = args.flatMap((arg) => {
+                    if (arg == null) {
+                        return [];
+                    } else {
+                        return htmlLike({
+                            tag: "h",
+                            content: arg,
+                        });
+                    }
+                });
                 return htmlLike({
                     tag: "idx",
-                    content: htmlLike({
-                        tag: "h",
-                        content: args[0] || [],
-                    }),
+                    content: formattedArgs,
                 });
             },
             includegraphics: (node) => {
@@ -94,30 +101,44 @@ function convert(value: string) {
     return output;
 }
 
-function replaceMath() {
-
-    return function (tree) {
-      visit(tree, node => node.type === "element", function (node) {
-        if (node.tagName === 'span' && node.properties.className.includes("inline-math")) {
-          node.tagName = 'm'
-        }
-        
-        if (node.tagName === 'div' && node.properties.className.includes("display-math")) {
-            node.tagName = "p";
-            node.properties.className = undefined;
-            const me: Hast.Element = {
-                type: "element",
-                tagName: "me",
-                children: node.children,
-            }
-            node.children = [me]
-        }
-      })
+function isHastElement(elm: any): elm is Hast.Element {
+    return elm && typeof elm === "object" && elm.type === "element";
+}
+function hasClass(elm: Hast.Element, className: string): boolean {
+    if (!isHastElement(elm)) {
+        return false;
     }
-  }
+    if (!Array.isArray(elm.properties.className)) {
+        return false;
+    }
+    return elm.properties.className.includes(className);
+}
+
+const replaceMath: Plugin<[], Hast.Root, Hast.Root> = function replaceMath() {
+    return function (tree: Hast.Root & { data: undefined }) {
+        visit(tree, isHastElement, function (node: Hast.Element) {
+            if (node.tagName === "span" && hasClass(node, "inline-math")) {
+                node.tagName = "m";
+                node.properties.className = undefined;
+            }
+
+            if (node.tagName === "div" && hasClass(node, "display-math")) {
+                node.tagName = "p";
+                node.properties.className = undefined;
+                const me: Hast.Element = {
+                    type: "element",
+                    tagName: "me",
+                    children: node.children,
+                    properties: {},
+                };
+                node.children = [me];
+            }
+        });
+    };
+};
 
 function testConvert() {
-    const source = `\\[\\Set{1,2,3}.\\]`;
+    const source = `\\index[symbol]{$\\in$}`;
     const converted = convert(source);
     process.stdout.write(
         chalk.green("Converted") +
