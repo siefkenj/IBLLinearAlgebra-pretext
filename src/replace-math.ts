f250716b19ce42f533504adc510c12565dc10380
OrhanBC <orhancangurel@gmail.com>
2023-11-28 16:12:05 -0500

    Added comments and docstrings

--- src/replace-math.ts
+++ src/replace-math.ts
@@ -2,9 +2,16 @@ import { Plugin } from "unified";
 import { visit } from "unist-util-visit";
 import * as Hast from "hast";
 
+/**
+ * Check if an element is of type 'Hast.Element'.
+ */
 function isHastElement(elm: any): elm is Hast.Element {
     return elm && typeof elm === "object" && elm.type === "element";
 }
+
+/**
+ * Check if a HAST element has the input class name
+ */
 function hasClass(elm: Hast.Element, className: string): boolean {
     if (!isHastElement(elm)) {
         return false;
@@ -15,24 +22,33 @@ function hasClass(elm: Hast.Element, className: string): boolean {
     return elm.properties.className.includes(className);
 }
 
+/**
+ * This plugin modifies a HAST tree by replacing '<div>...<\div>' tags that have class name "display-math" with '<me>...<\me>' tags
+ * wrapped in '<p>...<\p>' tags, and '<span>...<\span>' tags that have class name "inline-math" with '<m>...<\m>' tags.
+ */
 export const replaceMath: Plugin<[], Hast.Root, Hast.Root> =
     function replaceMath() {
         return function (tree: Hast.Root & { data: undefined }) {
+            // Visity every node that is a HAST element
             visit(tree, isHastElement, function (node: Hast.Element) {
+                // Replace elements that have class name "inline-math" with '<m>...<\m>' tags.
                 if (node.tagName === "span" && hasClass(node, "inline-math")) {
                     node.tagName = "m";
                     node.properties.className = undefined;
                 }
-
+                // Replace elements that have class name "display-math" with '<m>...<\m>' <me>...<\me>' tags
+                // wrapped in '<p>...<\p>' tags.
                 if (node.tagName === "div" && hasClass(node, "display-math")) {
                     node.tagName = "p";
                     node.properties.className = undefined;
+                    // Create a HAST element with the children of the current node as its children.
                     const me: Hast.Element = {
                         type: "element",
                         tagName: "me",
                         children: node.children,
                         properties: {},
                     };
+                    // Wrap everything in '<p>...<\p>' tags.
                     node.children = [me];
                 }
             });
--- src/split-on-headings.ts
+++ src/split-on-headings.ts
@@ -4,28 +4,41 @@ import { htmlLike } from "@unified-latex/unified-latex-util-html-like";
 import { wrapPars } from "@unified-latex/unified-latex-to-hast";
 import { splitOnMacro } from "@unified-latex/unified-latex-util-split";
 
+/**
+ * This plugin splits a LaTeX AST on '\Heading{}' macros,
+ * puts the argument in '\html-tag:title{...}' html-like macros,
+ * wraps any paragraphs in '\html-tag:p{...}' html-like macros,
+ * and then finally wraps the content between the current '\Heading{}' macro
+ * and the next one in '\html-tag:section{...}' html-like macros.
+ */
 export const splitOnHeadings: Plugin<[], Ast.Root, Ast.Root> =
     function splitOnHeadings() {
         return function (ast: Ast.Root) {
+            // Split the AST on '\Heading{}' macros
             const split = splitOnMacro(ast.content, "Heading");
 
+            // Replace '\Heading{}' macros with '\html-tag:title{...}' html-like macros
             const newHeadings = split.macros.map((heading) =>
                 htmlLike({ tag: "title", content: heading.args![0].content })
             );
+            // Wrap any paragraphs in '\html-tag:p{...}' html-like macros
             const pars = split.segments.map((seg) => wrapPars(seg));
             let sections: Ast.Root["content"] = [];
+            // Procced only if there are \Heading{}' macros in the original AST tree.
             if (newHeadings.length > 0) {
                 for (let i = 0; i < newHeadings.length; i++) {
                     let sectionContent: Ast.Node[] = [newHeadings[i]];
                     for (let j = 0; j < pars[i + 1].length; j++) {
                         sectionContent.push(pars[i + 1][j]);
                     }
+                    // Wrap everything in in '\html-tag:section{...}' html-like macros.
                     const section = htmlLike({
                         tag: "section",
                         content: sectionContent,
                     });
                     sections.push(section);
                 }
+                // Manipulate the AST tree with the changes
                 ast.content = sections;
             }
         };
