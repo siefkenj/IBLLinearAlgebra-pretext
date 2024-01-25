4c52ce642c84f3e782b8d8640055d729b6d7af1c
OrhanBC <orhancangurel@gmail.com>
2024-01-24 19:01:28 -0500

    Added medskip, bigskip and noindent to ignored elements

--- src/plugin-replace-ignored-elements.ts
+++ src/plugin-replace-ignored-elements.ts
@@ -3,6 +3,9 @@ import * as Ast from "@unified-latex/unified-latex-types";
 import { replaceNode } from "@unified-latex/unified-latex-util-replace";
 import { match } from "@unified-latex/unified-latex-util-match";
 
+/**
+ * This plugin removes LaTeX elements that have no significance in PreTeXt.
+ */
 export const replaceIgnoredElements: Plugin<[], Ast.Root, Ast.Root> =
     function replaceIgnoredElements() {
         return function (ast) {
@@ -10,12 +13,17 @@ export const replaceIgnoredElements: Plugin<[], Ast.Root, Ast.Root> =
                 if (
                     match.macro(node, "hfill") ||
                     match.macro(node, "smallskip") ||
+                    match.macro(node, "medskip") ||
+                    match.macro(node, "bigskip") ||
                     match.macro(node, "emptybox") ||
-                    match.environment(node, "center")
+                    match.macro(node, "noindent")
                 ) {
                     return null;
-                } else if (match.environment(node, "minipage")) {
-                    return (node as Ast.Environment).content;
+                } else if (
+                    match.environment(node, "minipage") ||
+                    match.environment(node, "center")
+                ) {
+                    return node.content;
                 }
             });
         };
