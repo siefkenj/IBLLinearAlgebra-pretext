a548a8268356ef2fec5f9689b9288027c1b89c8d
OrhanBC <orhancangurel@gmail.com>
2024-02-09 00:58:20 -0500

    Added subsections to sections

--- src/plugin-split-on-headings.ts
+++ src/plugin-split-on-headings.ts
@@ -3,6 +3,7 @@ import * as Ast from "@unified-latex/unified-latex-types";
 import { htmlLike } from "@unified-latex/unified-latex-util-html-like";
 import { wrapPars } from "@unified-latex/unified-latex-to-hast";
 import { splitOnMacro } from "@unified-latex/unified-latex-util-split";
+import { s } from "@unified-latex/unified-latex-builder";
 
 /**
  * This plugin splits a LaTeX AST on '\Heading{}' macros,
@@ -32,9 +33,69 @@ export const splitOnHeadings: Plugin<[], Ast.Root, Ast.Root> =
             if (newHeadings.length > 0) {
                 for (let i = 0; i < newHeadings.length; i++) {
                     let sectionContent: Ast.Node[] = [newHeadings[i]];
-                    for (let j = 0; j < pars[i + 1].length; j++) {
-                        sectionContent.push(pars[i + 1][j]);
+                    const subsectionSplit = splitOnMacro(
+                        split.segments[i + 1],
+                        "subsubsection"
+                    );
+                    if (subsectionSplit.macros.length > 0) {
+                        sectionContent.push(
+                            htmlLike({
+                                tag: "introduction",
+                                content: wrapPars(subsectionSplit.segments[0]),
+                            })
+                        );
+                        for (
+                            let j = 1;
+                            j < subsectionSplit.segments.length;
+                            j++
+                        ) {
+                            const title = htmlLike({
+                                tag: "title",
+                                content:
+                                    subsectionSplit.macros[j - 1].args![3]
+                                        .content,
+                            });
+                            for (
+                                let k = 0;
+                                k < subsectionSplit.segments[j].length;
+                                k++
+                            ) {
+                                // subsectionContent.push(
+                                //     wrapPars(subsectionSplit.segments[j], {
+                                //         macrosThatBreakPars: [
+                                //             "SavedDefinitionRender",
+                                //         ],
+                                //     })[k]
+                                // );
+                            }
+                            const subsection = htmlLike({
+                                tag: "subsection",
+                                content: [
+                                    title,
+                                    ...wrapPars(subsectionSplit.segments[j]),
+                                ],
+                            });
+                            sectionContent.push(subsection);
+                        }
+                    } else {
+                        for (let j = 0; j < split.segments[i + 1].length; j++) {
+                            // sectionContent.push(pars[i + 1][j]);
+                            // sectionContent.push(
+                            //     wrapPars(split.segments[i + 1], {
+                            //         macrosThatBreakPars: [
+                            //             "SavedDefinitionRender",
+                            //         ],
+                            //     })[j]
+                            // );
+                            // sectionContent.push(split.segments[i + 1][j]);
+                        }
+                        sectionContent.push(
+                            ...wrapPars(split.segments[i + 1], {
+                                macrosThatBreakPars: ["SavedDefinitionRender"],
+                            })
+                        );
                     }
+
                     // Wrap everything in in '\html-tag:section{...}' html-like macros.
                     const section = htmlLike({
                         tag: "section",
